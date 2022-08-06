7c1167317a5ab77246ab4da2ff6035ae9045cace
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-08-06 15:25:22 +0600

    up down reducer

---
+++ src/Store/reducers/upDown.js
@@ -0,0 +1,15 @@
+const initialSate = 0;
+
+const changeNumber = (state = initialSate, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+
+    default:
+      return state;
+  }
+};
+
+export default changeNumber;
