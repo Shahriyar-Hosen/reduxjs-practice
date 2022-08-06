fcfc16fc94a15b7a0b15455237ed52584ff6b9ca
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-08-06 15:24:56 +0600

    action type

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import "./App.css";
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div>
       <h1>Hello Redux</h1>
     </div>
   );
-}
+};
 
 export default App;
